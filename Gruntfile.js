6aae5dc3061af80ebd5acd4923b1e35cd923d0d1
aaron <a.aa.akio.aaron@gmail.com>
2013-10-07 23:54:50 -0700

    Post-Client/Server-refactor Epic Progress Commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -52,7 +52,7 @@ module.exports = function(grunt) {
       },
       build: {
         files: {
-          '<%= meta.build.js %>/app.js': ['./lib/launchClient.js'],
+          '<%= meta.build.js %>/app.js': ['./lib/app.js'],
         },
       },
     },
@@ -223,16 +223,41 @@ module.exports = function(grunt) {
 
   // Launch voxel-server
   grunt.registerTask('host', function() {
+    var port = 8000
+
+    // dependencies
+    var http = require('http')
+    var path = require('path')
+    var ecstatic = require('ecstatic')
+    var WebSocketServer = require('ws').Server
+    var websocket = require('websocket-stream')
+    var duplexEmitter = require('duplex-emitter')
+    var Server = require('./lib/server.js')
+
+    // create server
+    var server = new Server({
+      worldId: 'kumavis',
+    })
+    // setup WebSocketServer
     grunt.log.write('Starting host server.')
-    var server = require('./lib/launchServer.js')(),
-        serverPort = 8000
-    server.listen(serverPort)
-    grunt.log.write('Server Listening on ', serverPort)
+    var httpServer = http.createServer(ecstatic(path.join(__dirname, 'www')))
+    var wss = new WebSocketServer({server: httpServer})
+    httpServer.listen(port)
+    wss.on('connection',function(ws) {
+      var stream = websocket(ws)
+      var connection = duplexEmitter(stream)
+      server.connectClient(connection)
+      // handle connection end/error
+      stream.once('end', function(){ server.removeClient(connection) })
+      stream.once('error', function(){ server.removeClient(connection) })
+    })
+    grunt.log.write('Server Listening on ', port)
   })
 
   // Keep alive
-  var fs = require('fs');
   grunt.registerTask('keepalive', function() {
+    var fs = require('fs')
+
     var pidfile = './grunt.pid'
     fs.writeFileSync(pidfile,process.pid)
     grunt.log.write('Keeping Grunt task alive.\n')
