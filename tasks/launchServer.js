f45fe3aeeb3edb7757f61924984962ae9246af13
aaron <a.aa.akio.aaron@gmail.com>
2013-10-30 01:17:04 -0700

    Build - Fixes #18
    https://github.com/kumavis/node-warrior/issues/18

--- tasks/launchServer.js
+++ tasks/launchServer.js
@@ -5,23 +5,37 @@ var ecstatic = require('ecstatic')
 var WebSocketServer = require('ws').Server
 var websocket = require('websocket-stream')
 var duplexEmitter = require('duplex-emitter')
+var level = require('level')
+var sublevel = require('level-sublevel')
+// voxel dependencies
+var voxelLevel = require('voxel-level')
+// local dependencies
 var Server = require('../lib/server.js')
 
 module.exports = function(grunt) {
 
+  // hard coded options (temporary)
+  var worldId = 'kumavis'
+    , dbPath = path.resolve(__dirname,'../world/',worldId)
+    , serverPort =  8000
+
   // Launch voxel-server
   grunt.registerTask('launchServer', function() {
-    var port = 8000
+
+    // setup fancy db
+    grunt.log.write('using db: '+dbPath)
+    var voxelDb = voxelLevel(sublevel(level(dbPath)))
 
     // create server
     var server = new Server({
-      worldId: 'kumavis',
+      worldId: worldId,
+      voxelDb: voxelDb,
     })
     // setup WebSocketServer
     grunt.log.write('Starting host server.')
     var httpServer = http.createServer(ecstatic(path.join(__dirname, 'www')))
     var wss = new WebSocketServer({server: httpServer})
-    httpServer.listen(port)
+    httpServer.listen(serverPort)
     wss.on('connection',function(ws) {
       var stream = websocket(ws)
       var connection = duplexEmitter(stream)
@@ -30,7 +44,8 @@ module.exports = function(grunt) {
       stream.once('end', function(){ server.removeClient(connection) })
       stream.once('error', function(){ server.removeClient(connection) })
     })
-    grunt.log.write('Server Listening on ', port)
+    grunt.log.write('Server Listening on ', serverPort)
+    
   })
 
 }
